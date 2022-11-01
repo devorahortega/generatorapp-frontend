2a855f92a7d69fd21dccddee6bf156cb58bb37fd
Devorah Ortega <devorahb.ortega@gmail.com>
2022-11-01 13:33:16 -0500

    Attempts to rework the bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/home">Home</router-link>
       |
       <router-link to="/about">About</router-link>
+      |
+      <router-link to="/contact">Contact</router-link>
     </div>
     <router-view />
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center">
+      <div class="container d-flex align-items-center justify-content-between">
+        <h1 class="logo"><a href="index.html">GeneratorApp</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li><a class="nav-link scrollto active" href="/home">Home</a></li>
+            <li><a class="nav-link scrollto" href="/about">About</a></li>
+            <li><a class="nav-link scrollto" href="/contact">Contact</a></li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+        <!-- .navbar -->
+      </div>
+    </header>
+    <!-- End Header -->
     <!-- ======= Footer ======= -->
     <footer id="footer">
       <div class="footer-top">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+    <!-- ======= Top Bar ======= -->
+    <section id="topbar" class="d-flex align-items-center">
+      <div class="container d-flex justify-content-center justify-content-md-between">
+        <div class="contact-info d-flex align-items-center">
+          <i class="bi bi-envelope-fill"></i>
+          <a href="mailto:contact@example.com">info@example.com</a>
+          <i class="bi bi-phone-fill phone-icon"></i>
+          +1 5589 55488 55
+        </div>
+        <div class="social-links d-none d-md-block">
+          <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        </div>
+      </div>
+    </section>
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+      <div class="container position-relative" data-aos="fade-up" data-aos-delay="500">
+        <h1>Click to Generate!</h1>
+        <h2>Click on this button to generate a random word</h2>
+        <a href="#about" class="btn-get-started scrollto">Generate</a>
+      </div>
+    </section>
+    <!-- End Hero -->
   </div>
 </template>
 
